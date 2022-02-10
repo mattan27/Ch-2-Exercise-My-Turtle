5d8f8ebd5a0200b70901204e6daf019999b880cd
Matthew-JJ3 <98533759+mattan27@users.noreply.github.com>
2022-02-10 14:51:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+"""
+This is the Template Repl for Python with Turtle.
+
+Python with Turtle lets you make graphics easily in Python.
+
+Check out the official docs here: https://docs.python.org/3/library/turtle.html
+"""
+
+import turtle
+
+my_turtle = turtle.Turtle()
+my_turtle.color("purple")
+my_turtle.shape("turtle")
+my_turtle.forward(50)
+my_turtle.left(90)
+my_turtle.forward(50)
+my_turtle.left(90)
+my_turtle.forward(50)
+my_turtle.left(90)
+my_turtle.forward(50)
+my_turtle.left(90)
+my_turtle.penup()
+my_turtle.color("red")
+my_turtle.right(90)
+my_turtle.forward(100)
+my_turtle.pendown()
+my_turtle.forward(50)
+my_turtle.left(90)
+my_turtle.forward(50)
+my_turtle.left(90)
+my_turtle.forward(50)
+my_turtle.left(90)
+my_turtle.forward(50)
+my_turtle.left(90)
+my_turtle.forward(50)
---
+++ secondary.py
@@ -0,0 +1,14 @@
+import turtle
+
+my_turtle = turtle.Turtle()
+my_turtle.color("purple")
+my_turtle.shape("turtle")
+my_turtle.forward(50)
+my_turtle.left(90)
+my.turtle.forward(50)
+my_turtle.left(90)
+my.turtle.forward(50)
+my_turtle.left(90)
+my.turtle.forward(50)
+my.turtle.right(50)
+my_turtle.color("red")
